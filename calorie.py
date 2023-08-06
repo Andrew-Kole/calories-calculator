bac00ef9a54dfc0851dd13e22ae7b7f7a0c48a75
pakitowka <whitesaint12@gmail.com>
2023-08-06 18:50:35 +0200

    Empty classes

---
+++ calorie.py
@@ -0,0 +1,13 @@
+class Calorie:
+    """
+    Represents amount of calculated calories
+    """
+
+    def __init__(self, weight, height, age, temperature):
+        self.weight = weight
+        self.height = height
+        self.age = age
+        self.temperature = temperature
+
+    def calculate(self):
+        pass
---
+++ temperature.py
@@ -0,0 +1,11 @@
+class Temperature:
+    """
+    Represents temperature extracted from timeanddate.com/weather webpage.
+    """
+
+    def __init__(self, country, city):
+        self.country = country
+        self.city = city
+
+    def get(self):
+        pass
